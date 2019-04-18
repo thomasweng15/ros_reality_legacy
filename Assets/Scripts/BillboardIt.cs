27e37c9c368903e92afeaa69683a23f029e1720a
Tiffany Chen <thchen@andrew.cmu.edu>
2019-04-18 00:42:07 -0400

    separate 2 tables, new labels, new cube setup

---
+++ Assets/Scripts/BillboardIt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BillboardIt : MonoBehaviour {
+
+	// Use this for initialization
+
+	private GameObject cam;
+	void Start () {
+		cam = GameObject.FindWithTag("MainCamera");
+		print("cam is " + cam);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 this.transform.rotation = cam.transform.rotation;
+		 print("cam.transform is " + cam.transform.rotation);
+	}
+}
+
