a5ab5667e9f1c2accd47807ef0dccc7cfd07bcb3
Tiffany Chen <thchen@andrew.cmu.edu>
2019-04-21 22:06:28 -0400

    Trackpad input received and sectioned off

---
+++ Assets/RadialMenuButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	public bool selectedState = false;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/NondominantControls.cs
+++ Assets/Scripts/NondominantControls.cs
@@ -4,17 +4,115 @@ using UnityEngine;
 
 public class NondominantControls : MonoBehaviour {
 
-	// Use this for initialization
-	public Transform prefab;
+	private SteamVR_TrackedObject objTracked;
+	private SteamVR_Controller.Device device;
+	private SteamVR_TrackedController controller;
+
+	public GameObject[] buttonArr;
+
 	void Start () {
-		
+		objTracked = GetComponent<SteamVR_TrackedObject>();
+		controller = GetComponent<SteamVR_TrackedController>();
 	}
-	
-	// Update is called once per frame
 	void Update () {
+		device = SteamVR_Controller.Input((int)objTracked.index);
+		// Grabs input again each time update loop is called
+
 		if (Input.GetAxis("Left Trigger") > 0.5f)
         {
 			Debug.Log("Left Trigger Press");
         }
+
+		/* Controller touchpad is a unit circle */
+		if(controller.padPressed){
+			// int optionSelected = quadSelector();
+			// switch (optionSelected){
+			// 	case 0:
+			// 		break;
+			// 	case 1:
+			// 	//redo
+			// 		break;
+			// 	case 2:
+			// 	//undo
+			// 		break;
+			// 	case 3:
+			// 	//reset
+			// 		break;
+
+			// 	default:
+			// 		break;
+			// }
+
+		}
+		if(controller.padTouched){
+			int optionSelected = quadSelector();
+			print("optionSelected: " + optionSelected);
+			//button highlighting by chan ging hte albedo
+			
+			// print("selectedState: " + buttonArr[optionSelected].GetComponent<RadialButtonState>().selectedState);
+			// //bool selectedState = buttonArr[optionSelected].GetComponent<ButtonBehavior>().selectedState;
+			// for(int i= 0; i < buttonArr.Length; i ++){
+			// 	bool selectedState = buttonArr[i].GetComponent<RadialButtonState>().selectedState;
+			// 	print("the button's selectedState is "+ selectedState);
+			// 	if (i == optionSelected){
+			// 		if(!selectedState){
+			// 			buttonArr[optionSelected].GetComponent<Transform>().localScale *=1.01f;
+			// 		}
+			// 		buttonArr[optionSelected].GetComponent<RadialButtonState>().selectedState = true;
+			// 	}else{
+			// 		if(selectedState){
+			// 			buttonArr[optionSelected].GetComponent<Transform>().localScale /= 1.01f;
+			// 		}
+			// 		buttonArr[optionSelected].GetComponent<RadialButtonState>().selectedState = false;
+			// 	}
+			// }
+			// switch (optionSelected){
+			// 	case 0:
+			// 		break;
+			// 	case 1:
+			// 	//redo
+			// 		break;
+			// 	case 2:
+			// 	//undo
+			// 		break;
+			// 	case 3:
+			// 	//reset
+			// 		break;
+
+			// 	default:
+			// 		break;
+			// }
+		}
+	}
+
+	private int quadSelector(){
+		/* Touchpad is basically a unit circle from the device axises x and y */
+		float x = device.GetAxis().x;
+		print("x: " + x);
+		float y = device.GetAxis().y;
+		print("y: " + y);
+		float angle = Mathf.Abs(Mathf.Atan(y / x) * 180 / Mathf.PI);
+
+		/* Bottom half of touchpad mirrors the top half, so we only need to check x. */
+		if(x < 0){
+			angle = 180 - angle;
+		}
+
+		/* If the x is negative, that means, whatever the angle value, 
+		*  it's actually 180-angle whereas if x is positive, it's just angle.
+		*/
+
+		print("ANGLE: " + angle);
+		if(angle <= 45 && angle > 0){
+			return 0;
+		}else if( angle > 45 && angle <=90){
+			return 1;
+		}else if (angle > 90 && angle <= 135){
+			return 2;
+		}else if (angle > 135 && angle <= 180){
+			return 3;
+		}else{
+			return -1;
+		}
 	}
 }
