06371814287c373b881aba10c3137a7740eb787c
Tiffany Chen <thchen@andrew.cmu.edu>
2019-04-25 00:41:35 -0400

    rewrote web socket, needs to be tested

--- Assets/Scripts/ArmController.cs
+++ Assets/Scripts/ArmController.cs
@@ -1,122 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class ArmController : MonoBehaviour {
-    // string of which arm to control. Valid values are "left" and "right"
-    public string arm;
-
-    private string grip_label;
-    private string trigger_label;
     //websocket client connected to ROS network
     private WebsocketClient wsc;
-    TFListener TFListener;
-    //scale represents how resized the virtual robot is
-    float scale;
-
+   
     //the end effector that holds the array of ghost transforms
     private GameObject electricFingers;
+    private bool playPressed;
 
     void Start() {
+        playPressed = false;
         electricFingers = GameObject.FindWithTag("EndEffector");
         // Get the live websocket client
         wsc = GameObject.Find("WebsocketClient").GetComponent<WebsocketClient>();
 
-        // Get the live TFListener
-        TFListener = GameObject.Find("TFListener").GetComponent<TFListener>();
-
         // Create publisher to the Baxter's arm topic (uses Ein)
-        wsc.Advertise("ein/" + arm + "/forth_commands", "std_msgs/String");
-        // Asychrononously call sendControls every .1 seconds
-        InvokeRepeating("SendControls", .1f, .1f);
+        wsc.Advertise("ein/" + "right" + "/forth_commands", "std_msgs/String");
+    }
 
-        if (arm == "left") {
-            grip_label = "Left Grip";
-            trigger_label = "Left Trigger";
+    void Update(){
+        if(playPressed){
+            // Asychrononously call sendControls every .1 seconds
+            InvokeRepeating("SendControls", .1f, .1f);
+        } else {
+            CancelInvoke("SendControls"); 
         }
-        else if (arm == "right") {
-            grip_label = "Right Grip";
-            trigger_label = "Right Trigger";
-            Debug.Log("right grip");
-        }
-        else
-            Debug.LogError("arm variable is not set correctly");
     }
 
     void SendControls() {
-        scale = TFListener.scale;
+        List<String> commands = electricFingers.GetComponent<copyItself>().ghostCommands;
 
-        //Convert the Unity position of the hand controller to a ROS position (scaled)
-        Vector3 outPos = UnityToRosPositionAxisConversion(GetComponent<Transform>().position) / scale;
-        //Convert the Unity rotation of the hand controller to a ROS rotation (scaled, quaternions)
-        Quaternion outQuat = UnityToRosRotationAxisConversion(GetComponent<Transform>().rotation);
-        //construct the Ein message to be published
-        string message = "";
-        //Allows movement control with controllers if menu is disabled
-
-        //if deadman switch held in, move to new pose
-        if (Input.GetAxis(grip_label) > 0.5f) {
-            //construct message to move to new pose for the robot end effector 
-            message = outPos.x + " " + outPos.y + " " + outPos.z + " " +
-            outQuat.x + " " + outQuat.y + " " + outQuat.z + " " + outQuat.w + " moveToEEPose";
-            //if touchpad is pressed (Crane game), incrementally move in new direction
+        while(commands.Count > 0){
+            String message = commands[0];
+            commands.RemoveAt(0);
+            //Send the message to the websocket client (i.e: publish message onto ROS network)
+            wsc.SendEinMessage(message, "right");
         }
 
-        //If trigger pressed, open the gripper. Else, close gripper
-        if (Input.GetAxis(trigger_label) > 0.5f) {
-            message += " openGripper ";
-        }
-        else {
-            message += " closeGripper ";
-        }
-
-        //Send the message to the websocket client (i.e: publish message onto ROS network)
-        wsc.SendEinMessage(message, arm);
+        playPressed = false;
     }
-    void SendAGhost() {
-        scale = TFListener.scale;
-
-        //Convert the Unity position of the hand controller to a ROS position (scaled)
-        Vector3 outPos = UnityToRosPositionAxisConversion(GetComponent<Transform>().position) / scale;
-        //Convert the Unity rotation of the hand controller to a ROS rotation (scaled, quaternions)
-        Quaternion outQuat = UnityToRosRotationAxisConversion(GetComponent<Transform>().rotation);
-        //construct the Ein message to be published
-        string message = "";
-        //Allows movement control with controllers if menu is disabled
-
-        //if deadman switch held in, move to new pose
-        if (Input.GetAxis(grip_label) > 0.5f) {
-            //construct message to move to new pose for the robot end effector 
-            message = outPos.x + " " + outPos.y + " " + outPos.z + " " +
-            outQuat.x + " " + outQuat.y + " " + outQuat.z + " " + outQuat.w + " moveToEEPose";
-            //if touchpad is pressed (Crane game), incrementally move in new direction
-        }
-
-        //If trigger pressed, open the gripper. Else, close gripper
-        if (Input.GetAxis(trigger_label) > 0.5f) {
-            message += " openGripper ";
-        }
-        else {
-            message += " closeGripper ";
-        }
-
-        //Send the message to the websocket client (i.e: publish message onto ROS network)
-        wsc.SendEinMessage(message, arm);
-    }
-    //Convert 3D Unity position to ROS position 
-    Vector3 UnityToRosPositionAxisConversion(Vector3 rosIn) {
-        return new Vector3(-rosIn.x, -rosIn.z, rosIn.y);
-    }
-
-    //Convert 4D Unity quaternion to ROS quaternion
-    Quaternion UnityToRosRotationAxisConversion(Quaternion qIn) {
-
-        Quaternion temp = (new Quaternion(qIn.x, qIn.z, -qIn.y, qIn.w)) * (new Quaternion(0, 1, 0, 0));
-        return temp;
-
-        //return new Quaternion(-qIn.z, qIn.x, -qIn.w, -qIn.y);
-        //return new Quaternion(-qIn.z, qIn.w, -qIn.x, -qIn.y);
-        //return new Quaternion(-qIn.z, qIn.w, -qIn.x, -qIn.y);
-        //return new Quaternion(-qIn.z, qIn.x, qIn.w, qIn.y);
-    }
-
 }
 
--- Assets/Scripts/copyItself.cs
+++ Assets/Scripts/copyItself.cs
@@ -1,38 +1,88 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 public class copyItself : MonoBehaviour {
 
-	// Use this for initialization
+	 // string of which arm to control. Valid values are "left" and "right"
+    public string arm;
+    private string grip_label;
+    private string trigger_label;
+	 TFListener TFListener;
+    //scale represents how resized the virtual robot is
+    float scale;
 	public Transform prefab;
-
-	public List<Transform> ghostArray = new List<Transform>();
-    void Start()
+	public List<String> ghostCommands = new List<String>();
+    
+	void Start()
     {
-
+		if (arm == "left") {
+            grip_label = "Left Grip";
+            trigger_label = "Left Trigger";
+        }
+        else if (arm == "right") {
+            grip_label = "Right Grip";
+            trigger_label = "Right Trigger";
+            Debug.Log("right grip");
+        }
+        else
+            Debug.LogError("arm variable is not set correctly");
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-	//this is the old code that left ghost imprints on the trigger
-		// if (Input.GetAxis("Right Trigger") > 0)
-        // {
-        //     Instantiate(prefab, this.transform.position, this.transform.rotation);
-		// 	Debug.Log("yo yoy oyoyoyyyoyoyoooooo");
-		// 	Debug.Log(Input.GetJoystickNames());
-		// 	for (int i = 0; i < Input.GetJoystickNames().Length; i ++){
-		// 		string whatwhat = Input.GetJoystickNames()[i];
-		// 		Debug.Log(whatwhat);
-		// 	}
-        // }
         
 	}
 	internal void drawGhost(){
         Instantiate(prefab, this.transform.position, this.transform.rotation);
 		Transform currentTransform = GetComponent<Transform>();
-		ghostArray.Add(currentTransform);
+		addCommand(currentTransform);
 	}
+
+	private void addCommand(Transform ghost){
+		 scale = TFListener.scale;
+
+        //Convert the Unity position of the hand controller to a ROS position (scaled)
+        Vector3 outPos = UnityToRosPositionAxisConversion(GetComponent<Transform>().position) / scale;
+        //Convert the Unity rotation of the hand controller to a ROS rotation (scaled, quaternions)
+        Quaternion outQuat = UnityToRosRotationAxisConversion(GetComponent<Transform>().rotation);
+        //construct the Ein message to be published
+        string message = "";
+        //Allows movement control with controllers if menu is disabled
+
+        //if deadman switch held in, move to new pose
+        if (Input.GetAxis(grip_label) > 0.5f) {
+            //construct message to move to new pose for the robot end effector 
+            message = outPos.x + " " + outPos.y + " " + outPos.z + " " +
+            outQuat.x + " " + outQuat.y + " " + outQuat.z + " " + outQuat.w + " moveToEEPose";
+            //if touchpad is pressed (Crane game), incrementally move in new direction
+        }
+
+        //If trigger pressed, open the gripper. Else, close gripper
+        if (Input.GetAxis(trigger_label) > 0.5f) {
+            message += " openGripper ";
+        }
+        else {
+            message += " closeGripper ";
+        }
+	}
+
+	//Convert 3D Unity position to ROS position 
+    Vector3 UnityToRosPositionAxisConversion(Vector3 rosIn) {
+        return new Vector3(-rosIn.x, -rosIn.z, rosIn.y);
+    }
+
+    //Convert 4D Unity quaternion to ROS quaternion
+    Quaternion UnityToRosRotationAxisConversion(Quaternion qIn) {
+
+        Quaternion temp = (new Quaternion(qIn.x, qIn.z, -qIn.y, qIn.w)) * (new Quaternion(0, 1, 0, 0));
+        return temp;
+
+        //return new Quaternion(-qIn.z, qIn.x, -qIn.w, -qIn.y);
+        //return new Quaternion(-qIn.z, qIn.w, -qIn.x, -qIn.y);
+        //return new Quaternion(-qIn.z, qIn.w, -qIn.x, -qIn.y);
+        //return new Quaternion(-qIn.z, qIn.x, qIn.w, qIn.y);
+    }
 }
